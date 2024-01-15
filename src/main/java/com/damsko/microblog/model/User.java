60d5c7757fc444322171c4d976378de897919c17
Merry Damsko <m.d4msk0@gmail.com>
2024-01-15 16:12:30 +0100

    created User.java

--- src/main/java/com/damsko/microblog/model/User.java
+++ src/main/java/com/damsko/microblog/model/User.java
@@ -1,4 +1,28 @@
 package com.damsko.microblog.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public User(){}
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
 }
