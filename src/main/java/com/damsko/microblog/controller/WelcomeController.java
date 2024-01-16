12fd6d6a1624d71aa6c221148f6edfa777ba3e3f
Merry Damsko <m.d4msk0@gmail.com>
2024-01-17 00:33:57 +0100

    setting up slf4j and backlog, created logback-spring.xml

--- src/main/java/com/damsko/microblog/controller/WelcomeController.java
+++ src/main/java/com/damsko/microblog/controller/WelcomeController.java
@@ -1,13 +1,21 @@
 package com.damsko.microblog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
     @GetMapping("/")
     public String showWelcomePage() {
+        if (logger.isDebugEnabled()){
+            logger.debug("Showing Welcome page");
+        }
+        logger.info("Showing Welcome page");
         return "welcome";
     }
 }
